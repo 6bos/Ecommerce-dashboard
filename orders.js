1cf6c2d0720670a470e08caf9f815446fd097411
Maximiliano Dorado <97881449+6bos@users.noreply.github.com>
2022-10-17 19:06:16 -0300

    Add files via upload
    Se suben los archivos correspondientes desde el repositorio local

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName : 'HouseBirds - Marca: Perinola',
+        productNumber: '658923',
+        paymentStatus: 'Adeuda',
+        shipping: 'Pendiente'
+    },
+    {
+        productName : 'Alpiste "Cardenales" Bolsa x 20kg',
+        productNumber: '888329',
+        paymentStatus: 'Exitoso',
+        shipping: 'Enviado'
+    },
+    {
+        productName : 'Camita para Perros "Dream"',
+        productNumber: '903451',
+        paymentStatus: 'Exitoso',
+        shipping: 'Enviado'
+    },
+    {
+        productName : 'Heno para Conejos x 3kg',
+        productNumber: '676713',
+        paymentStatus: 'Rechazado',
+        shipping: 'Pendiente'
+    },
+    {
+        productName : 'Bebedero artesanal',
+        productNumber: '562311',
+        paymentStatus: 'Exitoso',
+        shipping: 'Pendiente'
+    },
+]
